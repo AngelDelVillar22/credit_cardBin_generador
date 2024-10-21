3faa167c9c4bafb8dded5976bb05c25cb587221c
AngelDelVillar22 <cv5293066@gmail.com>
2024-10-21 11:06:52 -0600

    credit_cardBin_generador.py

--- credit_cardBin_generador.py
+++ credit_cardBin_generador.py
@@ -1,27 +1,6 @@
-#!/usr/bin/env python
-#-*- coding: iso-8859-1 -*-
-import getopt
-import time
-import os
-import sys
+import random
 import datetime
-from random import randint
 
-version = "1.0.0"
-#Mensaje
-os.system ("clear")
-print("\033[1;32m+\033[1;36m GENERADOR")
-time.sleep(1.2)
-#Informacion de ayuda
-def angeldelvillar ():
-time.sleep(1.2)
-print("")
-print("\033[1;32m Informacion de Termux")
-print("")
-time.sleep(1.6)
-print(" https://t.me/master_Angel_Delvillar")
-print("")
-time.sleep(2.2)
 # Función para generar una fecha aleatoria entre 2023 y 2031
 generate_random_date() {
     local year=$((2023 + RANDOM % 9))
@@ -65,5 +44,4 @@ main() {
     done
 }
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+main
